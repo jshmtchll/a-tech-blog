35ef49d4cfb8cdaee2a39e01d6adac900d5c0695
josh mitchell <jshmtchll@gmail.com>
2021-05-17 11:31:37 -0500

    server and sequlize connection, starting on routes

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//import sequelize
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  Sequelize = new Sequelize(
+    process.env.DBNAME,
+    process.env.DBUSER,
+    process.env.DBPW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
